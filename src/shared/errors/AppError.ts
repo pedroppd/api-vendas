cf47e1f8b0327bd46e69043e42b31af80999ae0f
pedroppd <pedro_pdantas@hotmail.com>
2021-09-05 13:47:28 -0300

    Middleware de error

---
+++ src/shared/errors/AppError.ts
@@ -0,0 +1,9 @@
+export class AppError {
+    public readonly message: string;
+    public readonly statusCode: number;
+
+    constructor(message: string, status: number = 400) {
+        this.statusCode = status;
+        this.message = message;
+    }
+}
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,13 +1,21 @@
-import express from 'express';
+import express, {NextFunction, Request, Response} from 'express';
 import bodyParser from "body-parser";
 import cors from 'cors';
 import routes from "./routes";
+import { AppError } from '@shared/errors/AppError';
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use(routes);
+//Middleware de error
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    if(error instanceof AppError){
+       return res.status(error.statusCode).json({message: error.message});
+    }
+    return res.status(500).json({message: 'Internal server error'});
+});
 
 app.listen(5000, () => {
     console.log('Server is running');
-})
+});
