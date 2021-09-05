d9774febebd8209dd27cc7d6e338896e9805da31
pedroppd <pedro_pdantas@hotmail.com>
2021-09-05 12:06:24 -0300

    Configurando pastas e server.ts

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,13 @@
-console.log('Olá mundo');
+import express from 'express';
+import bodyParser from "body-parser";
+import cors from 'cors';
+import routes from "./routes";
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(routes);
+
+app.listen(5000, () => {
+    console.log('Server is running');
+})
